2a7d087771c35eeead466c1324e68e1fdb07484e
Jamal <jamal.hyderr@gmail.com>
2023-07-13 12:33:06 +0500

    text changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
         <header>
             <img src="./images/troll-face.png" />
             <h2>Meme Generator</h2>
-            <p>Build with React</p>
+            <p>Build with React, HTML & CSS</p>
         </header>
     )
 }
